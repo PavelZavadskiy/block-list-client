aa49d70e6d8e3ffe44e483f867a49305532d2415
Pavel <87939143+PavelZavadskiy@users.noreply.github.com>
2024-07-06 17:41:54 +0300

    Added react-query

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,10 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { AppProvider } from './app-prividers';
 
 export function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
+  );
 }
