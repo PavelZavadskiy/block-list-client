ca0af6be90c85c7c47faf9f52a79fd0fb44d8dde
Pavel <87939143+PavelZavadskiy@users.noreply.github.com>
2024-07-06 21:15:50 +0300

    Added UI components

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,8 +3,12 @@ import { Inter } from 'next/font/google';
 import { useEffect } from 'react';
 import { authControllerGetSessionInfo, authControllerSignIn } from '@/shared/api/generated';
 import { useQuery } from '@tanstack/react-query';
-
-const inter = Inter({ subsets: ['latin'] });
+import { UiButton } from '@/shared/ui/ui-button';
+import { UiTextField } from '@/shared/ui/ui-text-field';
+import { UiSelectField } from '@/shared/ui/ui-select-field';
+import { UiLink } from '@/shared/ui/ui-link';
+import { UiSpinner } from '@/shared/ui/ui-spinner';
+import { UiHeader } from '@/shared/ui/ui-header';
 
 export function HomePage() {
   const { data } = useQuery({
@@ -12,7 +16,27 @@ export function HomePage() {
     queryFn: () => authControllerGetSessionInfo(),
   });
 
-  return <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-    {data?.email}
-  </main>;
+  return (
+    <main className={`min-h-screen`}>
+      <UiHeader right={<div>{data?.email}</div>} />
+      <UiButton variant="primary">Hey</UiButton>
+      <UiButton variant="secondary">Hey</UiButton>
+      <UiButton variant="outlined">Sign Out</UiButton>
+      <UiButton disabled variant="primary">
+        Sign Out
+      </UiButton>
+      <UiTextField label="Text Field" inputProps={{ placeholder: 'Enter email...' }} />
+      <UiTextField error="Text Field" inputProps={{ placeholder: 'Enter email...' }} />
+      <UiTextField inputProps={{ placeholder: 'Enter email...' }} />
+      <UiSelectField
+        options={[
+          { value: '1', label: 'options 1' },
+          { value: '2', label: 'options 2' },
+        ]}
+      />
+      <UiLink href={'/'}>Link</UiLink>
+      <UiSpinner className="text-teal-600 w-20 h-20" />
+      {/* <UiPageSpinner /> */}
+    </main>
+  );
 }
